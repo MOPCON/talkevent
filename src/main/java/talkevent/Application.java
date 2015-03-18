f5a54b2e6e5b445b44a349e080c0ce751cd228b2
pcbill <pcbill@gmail.com>
2015-03-18 15:03:11 +0800

    add Application.java, and update build.gradle that about Sprint Boot

---
+++ src/main/java/talkevent/Application.java
@@ -0,0 +1,10 @@
+package talkevent;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScanpublic
+public class Application {
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
